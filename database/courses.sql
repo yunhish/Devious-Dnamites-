35e59d3dd3521b703f776ffc6485e154ad435aa5
bz0l <B.Zolana@Wlv.ac.uk>
2026-03-09 15:48:21 +0000

    Create courses.sql

---
+++ database/courses.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `courses` (
+ `id` int NOT NULL,
+ `course_code` int NOT NULL,
+ `title` varchar(200) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
