8981b1f8d41cbd3dabac7f5cf4c6fb4e4cc5a4db
bz0l <B.Zolana@Wlv.ac.uk>
2026-03-11 15:50:59 +0000

    Update courses.sql

--- database/sql/courses.sql
+++ database/sql/courses.sql
@@ -1,6 +1,10 @@
-CREATE TABLE `courses` (
- `id` int NOT NULL,
- `course_code` int NOT NULL,
- `title` varchar(200) NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+CREATE TABLE courses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    course_code INT NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    department_id INT,
+    
+    FOREIGN KEY (department_id) 
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+) ENGINE=InnoDB;
