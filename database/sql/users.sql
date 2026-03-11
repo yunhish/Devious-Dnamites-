b51d36ae95c31eef3cfbb0b1cc971caf69e7574c
bz0l <B.Zolana@Wlv.ac.uk>
2026-03-11 15:50:15 +0000

    Update users.sql

--- database/sql/users.sql
+++ database/sql/users.sql
@@ -1,10 +1,15 @@
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(120) UNIQUE,
-    password VARCHAR(255),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(120) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    department_id INT,
+
     verification_token VARCHAR(255),
     token_expiry DATETIME,
     is_verified TINYINT(1) DEFAULT 0,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
